f4b2f745d4f1194d66e9ea78847876e0a69d2223
Jared Knowles <jknowles@gmail.com>
2012-06-08 14:29:06 -0500

    Expand slides and reorganize

--- slidemake.R
+++ slidemake.R
@@ -1,3 +1,6 @@
 # Make slides script
 
-system("pandoc -s -S -i -t slidy useRknowlesSlides.md -o KnowlesuseR2012talk.html --self-contained")
+system("pandoc -s -S -t slidy useRknowlesSlides.md -o KnowlesuseR2012talk.html --self-contained")
+
+#PDF (doesn't work with HTML markup)
+system("pandoc -s useRknowlesSlides.md -V theme:Cambridge -o beamer.pdf")
